3a420494cf6a4348c45d34a3d61e7077861f89b1
Andrew Angus <andrew.angus@warwick.ac.uk>
2019-11-29 17:20:47 +0000

    Completed section of code for group project

--- pot_driver.f90
+++ pot_driver.f90
@@ -5,11 +5,22 @@ USE potential
 IMPLICIT NONE
 
 TYPE(run_data) :: rundata
+TYPE(axes) :: axes_
+TYPE(particle) :: particle_
+REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: density, pot
+REAL(REAL64) :: execution_time, dx_, dy_
 
+! Read inputs from command line
 CALL user_inputs(rundata)
 
-CALL make_grid(rundata)
+! Initialise axes, charge density, and particle position/velocity
+CALL problem_setup(rundata, axes_, particle_, density, dx_, dy_)
 
-!CALL calc_potential()
+! Use Gauss-Seigel method to evaluate grid potential
+CALL calc_potential(rundata, density, dx_, dy_, pot)
+
+! Print execution time
+CALL CPU_TIME(execution_time)
+print '(A,F0.2,A)', "Execution time: ", execution_time, "s"
 
 END PROGRAM
